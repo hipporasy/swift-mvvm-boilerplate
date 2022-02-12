208ad6e7236a65e07f7e1df78dd303f9caf55e61
Marasy Phi <hipporasy@gmail.com>
2022-02-12 18:09:04 +0700

    Initial Commit

---
+++ Fishy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fishy
+//
+//  Created by Marasy Phi on 12/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fishy/FishyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishyApp.swift
+//  Fishy
+//
+//  Created by Marasy Phi on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FishyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
